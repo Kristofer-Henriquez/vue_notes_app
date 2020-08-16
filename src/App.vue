3945ab5ca626bc8230bd86652096c794b6b9e938
Kristofer <kristoferhenriquez@gmail.com>
2020-08-16 10:19:42 -0500

    Indentation fix

--- src/App.vue
+++ src/App.vue
@@ -1,117 +1,117 @@
 <template>
-	 <div id="app">
-	   <toolbar />
-	   <note-container />
-	 </div>
+  <div id="app">
+    <toolbar />
+    <note-container />
+  </div>
 	</template>
 <script>
-  import Toolbar from "./components/Toolbar.vue";
-  import NoteContainer from "./components/NoteContainer.vue";
-	export default {
-	 name: 'app',
-   components: {
-     Toolbar,
-     NoteContainer
-   }
-	};
-	</script>
+import Toolbar from "./components/Toolbar.vue";
+import NoteContainer from "./components/NoteContainer.vue";
+export default {
+  name: 'app',
+  components: {
+    Toolbar,
+    NoteContainer
+  }
+};
+</script>
 	<style>
 	/* RESET */
 	* {
-	 margin: 0;
-	 padding: 0;
-	 border: 0;
-	 outline: none;
-	 box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  outline: none;
+  box-sizing: border-box;
 	}
 	/* LAYOUT */
 	#app {
-	 display: flex;
-	 flex-direction: column;
-	 min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 	}
 	.toolbar {
-	 padding: 0.5em;
+	padding: 0.5em;
 	}
 	.toolbar-button, .toolbar-search {
-	 padding: inherit;
-	 border-radius: 0.3em;
+	padding: inherit;
+	border-radius: 0.3em;
 	}
 	.toolbar-search {
-	 float: right;
+	float: right;
 	}
 	.note-container {
-	 display: flex;
-	 flex: 1;
+	display: flex;
+	flex: 1;
 	}
 	.note-selectors {
-	 flex: 0 0 13em;
+	flex: 0 0 13em;
 	}
 	.note-selector {
-	 padding: 1em;
+	padding: 1em;
 	}
 	.note-selector p {
-	 margin: 0;
+	margin: 0;
 	}
 	.note-editor {
-	 display: flex;
-	 flex: 1;
-	 flex-direction: column;
+	display: flex;
+	flex: 1;
+	flex-direction: column;
 	}
 	.note-editor-info {
-	 padding: 0.5em;
-	 text-align: center;
+	padding: 0.5em;
+	text-align: center;
 	}
 	.note-editor-input {
-	 display: flex;
-	 flex: 1;
-	 width: 100%;
-	 padding: 0 2em 0 2em;
+	display: flex;
+	flex: 1;
+	width: 100%;
+	padding: 0 2em 0 2em;
 	}
 	/* COLORS */
 	* {
-	 color: #454545;
-	 background-color: #FAFAF8;
-	}
-	.toolbar {
-	 background-color: #DCDADC;
+    color: #454545;
+    background-color: #FAFAF8;
+  }
+  .toolbar {
+    background-color: #DCDADC;
 	}
 	.toolbar-button {
-	 background-color: #FFFFFF;
+	background-color: #FFFFFF;
 	}
 	.toolbar-button:active {
-	 background-color: #AAAAAA;
+	background-color: #AAAAAA;
 	}
 	.note-selectors {
-	 border-right: 1px solid #DCDADC;
+	border-right: 1px solid #DCDADC;
 	}
 	.note-selector {
-	 border-bottom: 1px solid #DCDADC;
+	border-bottom: 1px solid #DCDADC;
 	}
 	.note-selector.active {
-	 background-color: #FCE18D;
+	background-color: #FCE18D;
 	}
 	.note-selector-title {
-	 background-color: inherit;
+	background-color: inherit;
 	}
 	.note-selector-timestamp {
-	 color: #626262;
-	 background-color: inherit;
+	color: #626262;
+	background-color: inherit;
 	}
 	.note-editor-info {
-	 color: #DCDADC;
+	color: #DCDADC;
 	}
 	/* TYPOGRAPHY */
 	body {
-	 font-family: sans-serif;
+	font-family: sans-serif;
 	}
 	.note-selector-title {
-	 font-weight: bold;
+	font-weight: bold;
 	}
 	.note-selector-timestamp {
-	 font-size: 0.7em;
+	font-size: 0.7em;
 	}
 	.note-editor, .note-editor-input {
-	 font-size: 0.9em;
+	font-size: 0.9em;
 	}
 	</style>
